93d3b86cebc57dd7bc7c37b1644d9d8c123e3b69
KSFirstUser <rusakk@mail.ru>
2019-06-27 23:34:13 +0300

    first try - fixed bottom

--- data/database.js
+++ data/database.js
@@ -1,10 +1,8 @@
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize('testHak2019','root', '1995op1995',
-{
-    dialect:'mysql',
-    host:'localhost',
-  
+const sequelize = new Sequelize('testHak2019', 'root', '1995op1995', {
+    dialect: 'mysql',
+    host: 'localhost',
 });
 
 module.exports = sequelize;
